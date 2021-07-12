d585886a10575785eacfe43f42d7749bdf9708ad
jean-baptiste.granet <jean-baptiste.granet@efrei.net>
2021-07-12 14:36:13 +0200

    Initial Commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CommandAPI.Models;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly CommandContext _context;
+
+        public CommandsController(CommandContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetCommandItems()
+        {
+            return _context.CommandItems;
+        }
+
+        //GET: api/commands/n
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandItem(int id)
+        {
+            var CommandItem = _context.CommandItems.Find(id);
+            if(CommandItem == null)
+            {
+                return NotFound();
+            }
+            return CommandItem;
+        }
+
+        //POST: api/commands
+        [HttpPost]
+        public ActionResult<Command> PostCommandItem(Command command)
+        {
+            _context.CommandItems.Add(command);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCommandItem", new Command{ Id = command.Id }, command);
+        }
+
+        //PUT: api/commands/n
+        [HttpPut("{id}")]
+        public ActionResult PutCommandItem(int id, Command command)
+        {
+            if(id != command.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(command).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        //DELETE: api/commands/n
+        [HttpDelete("{id}")]
+        public ActionResult<Command> DeleteCommandItem(int id)
+        {
+            var commandItem = _context.CommandItems.Find(id);
+            if(commandItem == null)
+            {
+                return NotFound();
+            }
+            _context.CommandItems.Remove(commandItem);
+            _context.SaveChanges();
+
+            return commandItem;
+        }
+        /*[HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }*/
+    }
+}
